5383ab374186ec79747e8a201f12a32fa025a52a
Josepantoja48971 <Josepantoja48971@gmail.com>
2026-05-09 16:36:04 -0500

    establish mysql connection

--- Backend/server.js
+++ Backend/server.js
@@ -5,3 +5,16 @@ require('dotenv').config();
 const app=express();
 app.use(cors());
 app.use(express.json());
+const myDB=mysql.createConnection({
+host:process.env. DB_Host,
+user:process.env.DB_USER,
+password:process.env.DB_PASSWORD,
+database:process.env.DB_NAME
+});
+myDB.connect((err)=>{
+if (err) throw err;
+});
+app.get('/',(req,res)=>{
+res.json({message:'Welcome to task manager josep'});
+});
+
