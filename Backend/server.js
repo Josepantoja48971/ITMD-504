67b571adecb32dc1a61df49c5951731c8275a6dc
Josepantoja48971 <Josepantoja48971@gmail.com>
2026-05-09 16:20:31 -0500

    set up express app

--- Backend/server.js
+++ Backend/server.js
@@ -2,3 +2,6 @@ const express=require('express');
 const mysql=require('mysql2');
 const mysql=require('cors');
 require('dotenv').config();
+const app=express();
+app.use(cors());
+app.use(express.json());
